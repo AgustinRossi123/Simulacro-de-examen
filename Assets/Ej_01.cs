725aabfcffcf5c05a367dca1683b3340c1668ebb
AgustinRossi123 <101335570+AgustinRossi123@users.noreply.github.com>
2022-04-07 13:49:24 -0300

    Initial commit

---
+++ Assets/Ej_01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_01 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1==num2)
+        {
+            Debug.Log("Los dos números son iguales");
+        }
+        else
+        {
+            Debug.Log("Los dos números son diferentes");
+        }
+    }
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej_02.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_02 : MonoBehaviour
+{
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float monto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float totaldelosproductos;
+        float dinerofaltante;
+        totaldelosproductos = producto1+producto2+producto3;
+        dinerofaltante = totaldelosproductos - monto;
+
+        if (totaldelosproductos>monto)
+        {
+            Debug.Log("El dinero para comprar los productos es insuficiente. Le falta $"+dinerofaltante);
+        }
+        else if(totaldelosproductos <= monto)
+        {
+            Debug.Log("El dinero para comprar los productos es suficiente");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
